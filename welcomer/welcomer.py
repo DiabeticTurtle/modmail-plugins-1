46e92c80e21dff9551151ff29d27b1ab83882e34
Bront <Brentholt_1@outlook.com>
2020-10-29 19:07:51 -0400

    Update welcomer.py

--- welcomer/welcomer.py
+++ welcomer/welcomer.py
@@ -91,7 +91,7 @@ class Welcomer(commands.Cog):
                 {'$set': {'welcomer': {'channel': str(channel.id), 'message': message}}},
                 upsert=True
             )
-            await ctx.send(f'Message sent to {channel.mention}')
+            await ctx.send(f'Message sent to {channel.mention} ')
         else:
             await ctx.send('Invalid welcome message syntax.')
 
